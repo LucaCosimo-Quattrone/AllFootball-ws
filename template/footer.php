11c25587764a683a97734cd250def1f19b90c93e
Luca Quattrone <54541769+LucaCosimo-Quattrone@users.noreply.github.com>
2019-09-24 13:05:52 +0200

    Implementazione client

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<br><br><br><br><br>
+<footer class="page-footer font-small bg-dark fixed-bottom">
+  <div class="footer-copyright text-center py-3 text-success">© 2019 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/" class="text-success"> AllFootball</a>
+  </div>
+</footer>
+</html>
